6ca57bb3317baab186fad11ba9e3a48800d5ec83
Ben Reinhart <benjreinhart@gmail.com>
2017-03-30 17:14:57 -0500

    Add basic spec to cover policy generation

--- __test__/S3Policy.test.js
+++ __test__/S3Policy.test.js
@@ -1,19 +1,18 @@
 import { S3Policy } from '../src/S3Policy'
 
 describe('S3Policy.generate', () => {
+  let options = {}
+
+  beforeEach(() => options = {
+    key: 'image.jpg',
+    bucket: 'my-s3-bucket',
+    contentType: 'image/jpg',
+    region: 'us-east-1',
+    accessKey: "AKIAA7AS6DHAD6ASN23",
+    secretKey: "pLx+brfx0u12ERMcJCfNAEOKH+bMk40ZVa7hh8",
+  })
 
   describe('required options', () => {
-    let options = {}
-
-    beforeEach(() => options = {
-      key: 'image.jpg',
-      bucket: 'my-s3-bucket',
-      contentType: 'image/jpg',
-      region: 'us-east-2',
-      accessKey: "AKIAA7AS6DHAD6ASN23",
-      secretKey: "pLx+brfx0u12ERMcJCfNAEOKH+bMk40ZVa7hh8",
-    })
-
     test('ensures `key` key is present', () => {
       Reflect.deleteProperty(options, 'key')
       expect(() => S3Policy.generate(options)).toThrow('Must provide `key` option with the object key')
@@ -39,4 +38,21 @@ describe('S3Policy.generate', () => {
       expect(() => S3Policy.generate(options)).toThrow('Must provide `secretKey` option with your AWSSecretKey')
     })
   })
+
+  it('generates the correct policy', () => {
+    const policy = S3Policy.generate(options)
+
+    expect(policy).toHaveProperty('key', 'image.jpg')
+    expect(policy).toHaveProperty('acl', 'public-read')
+    expect(policy).toHaveProperty('success_action_status', '201')
+    expect(policy).toHaveProperty('Content-Type', 'image/jpg')
+    expect(policy).toHaveProperty('X-Amz-Algorithm', 'AWS4-HMAC-SHA256')
+
+    // TODO: make these deterministic and test their value
+    expect(policy).toHaveProperty('Policy')
+    expect(policy).toHaveProperty('X-Amz-Credential')
+    expect(policy).toHaveProperty('X-Amz-Date')
+    expect(policy).toHaveProperty('X-Amz-Signature')
+  })
+
 })
