d3bec2496cab2a1e3682073ac00529f98b15d949
Jefferson Filho <jefferson_filho008@hotmail.com>
2017-04-17 13:08:09 +0100

    Remove https from bucket url to fix issue NSURLSession/NSURLConnection HTTP load failed (kCFStreamErrorDomainSSL, -9843 on iOS devices.

--- src/RNS3.js
+++ src/RNS3.js
@@ -38,7 +38,7 @@ export class RNS3 {
       contentType: file.type
     }
 
-    const url = `https://${options.bucket}.${options.awsUrl || AWS_DEFAULT_S3_HOST}`
+    const url = `http://${options.bucket}.${options.awsUrl || AWS_DEFAULT_S3_HOST}`
     const method = "POST"
     const policy = S3Policy.generate(options)
 
