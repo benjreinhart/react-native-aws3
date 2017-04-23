b7ec17c8d174877502aaffbf90613fc110ffe854
rickevry <rickard@rickardnilsson.com>
2017-04-23 15:59:38 +0200

    fetch instead of XMLHttpRequest
    putWithFetch shows that there is a bug in how XMLHttpRequest loads a
    video stream. Instead of uploading the video, it uploads the first
    frame (as an image).

--- src/RNS3.js
+++ src/RNS3.js
@@ -3,6 +3,7 @@
  */
 
 import { Request } from './Request'
+import { FetchRequest } from './FetchRequest'
 import { S3Policy } from './S3Policy'
 
 const AWS_DEFAULT_S3_HOST = 's3.amazonaws.com'
@@ -30,6 +31,7 @@ const setBodyAsParsedXML = (response) =>
   })
 
 export class RNS3 {
+
   static put(file, options) {
     options = {
       ...options,
@@ -47,4 +49,21 @@ export class RNS3 {
       .send()
       .then(setBodyAsParsedXML)
   }
-}
+
+  static putWithFetch(file, options) {
+    options = {
+      ...options,
+      key: (options.keyPrefix || '') + file.name,
+      date: new Date,
+      contentType: file.type
+    }
+
+    const url = `https://${options.bucket}.${options.awsUrl || AWS_DEFAULT_S3_HOST}`
+    const method = "POST"
+    const policy = S3Policy.generate(options)
+
+    return FetchRequest.create(url, method, policy)
+      .set("file", file)
+      .send()
+      .then(setBodyAsParsedXML)
+  }}
